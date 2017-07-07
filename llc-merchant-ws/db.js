41acbc9912eb4034650aedb13af2b51957e71858
Senthilkumar R <senthil.kumar@limitlesscircle.com>
2017-07-07 16:44:39 +0530

    changes on connection string

--- llc-merchant-ws/db.js
+++ llc-merchant-ws/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('llcwebdb', 'root', 'limitless', {
-    host: 'localhost',
+const connection = new Sequelize('llcwebdb', 'root', 'pmt11cd3', {
+    host: '139.59.15.249',
     dialect: 'mysql'
 });
 
