975d0fc1227263e744441863e6399691a4b1fac5
ramk_1011 <ram@limitlesscircle.com>
2017-08-26 18:29:44 +0530

    Commit 26/8/2017 - video uploader added and new video services added

--- llc-merchant-ws/upload-services/uploadRouters/uploadRouter.js
+++ llc-merchant-ws/upload-services/uploadRouters/uploadRouter.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var uploadRouter = express.Router();
 var ImageUploadController = require('./../uploadControllers/imageUploadController');
-var youTubeUploadController = require('./../uploadControllers/youTubeUploadController');
+var VideoUploadController = require('./../uploadControllers/videoUploadController');
 var ImageUploadService = require('./../awsUploadServices/imageUploadService');
 
 //Upload routes
 uploadRouter.route('/image/:uploadApp/:uploadTo/:userId').post(ImageUploadController.imageUpload);
-uploadRouter.route('/video/:uploadApp/:uploadTo/:userId').post(youTubeUploadController.uploadVideoInYoutube);
-
-
+uploadRouter.route('/video/:uploadApp/:uploadTo/:userId').post(VideoUploadController.videoUpload);
 
 //Delete routes
 uploadRouter.route('/image/delete').put(ImageUploadService.deleteImage);
