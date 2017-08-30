f6d684aa8834980eb45a0201a67f757ee3e8ba48
uma <uma.raja@limitlesscircle.com>
2017-08-30 14:53:28 +0530

    template_images

---
+++ llc-merchant-ws/smtp/notificationTemplate/templateModels/templateImagesModel.js
@@ -0,0 +1,38 @@
+const sequelize = require('sequelize');
+const db = require('./../../../db');
+const TemplateController = require('./../templateModels/templateModel');
+
+const TemplateImages = db.define('template_images',{
+    template_id : {
+        type : sequelize.INTEGER,
+        references : {
+            model : TemplateController,
+            key : "id"
+        },
+        allowNull : false
+    },
+    destination_type : {
+        type : sequelize.STRING(45),
+        allowNull : false
+    },
+    start_img : {
+        type : sequelize.STRING(100),
+        defalutvalue : ''
+    },
+    stop_img : {
+        type : sequelize.STRING(100),
+        defalutvalue : ''
+    },
+    client_name:{
+        type : sequelize.STRING(45),
+        allowNull: false
+    },
+    client_img: {
+        type : sequelize.STRING(100),
+        defalutvalue : ''
+    }
+}, {
+    timestamps : false,
+    freezeTableName : true
+});
+module.exports = TemplateImages;
