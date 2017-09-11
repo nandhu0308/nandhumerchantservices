64e3114e14af86261eba3510a03a7ed8219124fb
Nandini <nandini@limitlesscircle.com>
2017-09-11 13:03:58 +0530

    created haappy Event

---
+++ llc-merchant-ws/haappyEvent/haappyEventModel.js
@@ -0,0 +1,41 @@
+const sequelize = require('sequelize');
+const db = require('./../../db');
+
+const HaappyEvent = db.define('haappy_event',  {
+
+    ha_name : {
+        type: sequelize.STRING,
+        allowNull: false
+    },
+
+    ha_location : {
+        type : sequelize.STRING,
+        
+    },
+   
+    ha_type : {
+        type : sequelize.STRING
+    },
+
+    ha_is_active: {
+        type: sequelize.BOOLEAN,
+        defaultValue: 1
+    },
+
+    ha_time: {
+        type: sequelize.TIMESTAMPS,
+        
+    },
+
+    ha_date :{
+        type: sequelize.DATE,
+       
+    }
+    },
+    
+    {
+    timestamps : false,
+    freezeTableName : true
+})
+
+module.exports = HaappyEvent;
