10dc2b2aa7405b723e2809a845145d0f9fcc562b
ramk_1011 <ram@limitlesscircle.com>
2017-10-23 15:21:31 +0530

    Commit 23/10/2017 - youtube upload base source completed

--- llc-merchant-ws/upload-services/multiplatform-services/multiplatform-uploader-service.js
+++ llc-merchant-ws/upload-services/multiplatform-services/multiplatform-uploader-service.js
@@ -4,6 +4,16 @@ var s3 = new AWS.S3({ region: 'ap-south-1' });
 var multer = require('multer');
 var fs = require('fs');
 var path = require('path');
+var Youtube = require('youtube-video-api');
+var google_auth = require('google-cli-auth');
+
+var youtube = Youtube({
+    video: {
+        part: 'status,snippet'
+    },
+    email: 'developer.limitlesscircle@gmail.com',
+    password: 'H@appy$99'
+});
 
 var uploadVideo = function (req, res) {
     console.log('API works');
@@ -52,7 +62,43 @@ var uploadVideo = function (req, res) {
             } if (data) {
                 console.log(data.Location);
                 console.log(req.file.size);
-                
+
+                var params = {
+                    resource: {
+                        snippet: {
+                            title: 'test video',
+                            description: 'This is a test video uploaded via the YouTube API'
+                        },
+                        status: {
+                            privacyStatus: 'private'
+                        }
+                    }
+                };
+
+                require('google-cli-auth')({
+                    name: 'yt-upload-client',
+                    client_id: '541496688190-3047k21qs4r9t266jvpca9rf5sssos91.apps.googleusercontent.com',
+                    client_secret: 'NdXsmSIxaBjSDDjCIr2AD_8I',
+                    scope: [
+                        'https://www.googleapis.com/auth/youtube.upload'
+                    ]
+                }, function (gaerr, token) {
+                    console.log(token);
+                    youtube.authenticate('541496688190-3047k21qs4r9t266jvpca9rf5sssos91.apps.googleusercontent.com', 'NdXsmSIxaBjSDDjCIr2AD_8I', token, function (aerr, token) {
+                        if (aerr) {
+                            console.log(aerr);
+                        } else {
+                            console.log(token);
+                            youtube.insert(file, params, function (yterr, video) {
+                                if (yterr) {
+                                    console.log(yterr);
+                                } else {
+                                    console.log(video);
+                                }
+                            });
+                        }
+                    });
+                });
             }
         });
     });
